7b5d9ae5c635ef3ad984ab731cfcd0c299010961
koheiyamaguchi0203 <kohei.yamaguchi846@litalico.jp>
2019-06-22 19:44:42 +0900

    formのコンポーネントを作った

---
+++ todo-app/src/InsertTodo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class InsertTodo extends React.Component {
+  render() {
+    return (
+      <form>
+        <input />
+        <input type="submit" value="Todoを作成する" />
+      </form>
+    );
+  }
+}
+
+export default InsertTodo;
--- todo-app/src/TodoListPage.jsx
+++ todo-app/src/TodoListPage.jsx
@@ -1,28 +1,33 @@
 import React from "react";
+import InsertTodo from "./InsertTodo";
 
-const TodoListPage = props => {
-  return (
-    <React.Fragment>
-      <h1>TodoApplication</h1>
-      <h2>InsertTodo</h2>
-      <div>
-        <input />
-        <input type="submit" value="Todoを追加する" />
-      </div>
-      <h2>SortTodo</h2>
-      <div>
-        <div>Todoのものだけ表示する</div>
-        <div>Archiveされたものだけを表示する</div>
-        <div>全て表示する</div>
-      </div>
-      <h2>ListTodo</h2>
-      <div>
-        {props.todoItems.map((todoItem, index) => (
-          <div key={index}>{todoItem.title}</div>
-        ))}
-      </div>
-    </React.Fragment>
-  );
-};
+class TodoListPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { ...props };
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1>TodoApplication</h1>
+        <h2>InsertTodo</h2>
+        <InsertTodo />
+        <h2>SortTodo</h2>
+        <div>
+          <div>Todoのものだけ表示する</div>
+          <div>Archiveされたものだけを表示する</div>
+          <div>全て表示する</div>
+        </div>
+        <h2>ListTodo</h2>
+        <div>
+          {this.state.todoItems.map((todoItem, index) => (
+            <div key={index}>{todoItem.title}</div>
+          ))}
+        </div>
+      </React.Fragment>
+    );
+  }
+}
 
 export default TodoListPage;
