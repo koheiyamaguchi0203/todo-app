09d04923841e59627e2d598d0da8714af3442fba
koheiyamaguchi0203 <kohei.yamaguchi846@litalico.co.jp>
2019-07-21 14:12:25 +0900

    不要なjsonをコメントアウトした

--- src/index.js
+++ src/index.js
@@ -4,36 +4,36 @@ import "./index.css";
 import TodoListPage from "./TodoListPage";
 import * as serviceWorker from "./serviceWorker";
 
-const date = new Date()
+// const date = new Date()
 
-const todoItems = [
-  {
-    id: 1,
-    title: "買い物に行く",
-    archived: false,
-    createdAt: new Date(date.setDate(10)).getTime()
-  },
-  {
-    id: 2,
-    title: "デートする",
-    archived: false,
-    createdAt: new Date(date.setDate(9)).getTime()
-  },
-  {
-    id: 3,
-    title: "サバゲーする",
-    archived: false,
-    createdAt: new Date(date.setDate(8)).getTime()
-  },
-  {
-    id: 4,
-    title: "プログラミングする",
-    archived: false,
-    createdAt: new Date(date.setDate(7)).getTime()
-  }
-];
+// const todoItems = [
+//   {
+//     id: 1,
+//     title: "買い物に行く",
+//     archived: false,
+//     createdAt: new Date(date.setDate(10)).getTime()
+//   },
+//   {
+//     id: 2,
+//     title: "デートする",
+//     archived: false,
+//     createdAt: new Date(date.setDate(9)).getTime()
+//   },
+//   {
+//     id: 3,
+//     title: "サバゲーする",
+//     archived: false,
+//     createdAt: new Date(date.setDate(8)).getTime()
+//   },
+//   {
+//     id: 4,
+//     title: "プログラミングする",
+//     archived: false,
+//     createdAt: new Date(date.setDate(7)).getTime()
+//   }
+// ];
 ReactDOM.render(
-  <TodoListPage todoItems={todoItems} />,
+  <TodoListPage />,
   document.getElementById("root")
 );
 
