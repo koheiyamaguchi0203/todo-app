001e142e4b589e3fce8520d89a924d019194d19f
koheiyamaguchi0203 <kohei.yamaguchi846@litalico.co.jp>
2019-07-21 17:17:03 +0900

    routesを移した

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+/*
+todo_path  GET     /api/v1/todos                          TodoAppApiWeb.TodoController :index
+todo_path  GET     /api/v1/todos/:id                      TodoAppApiWeb.TodoController :show
+todo_path  POST    /api/v1/todos                          TodoAppApiWeb.TodoController :create
+todo_path  PATCH   /api/v1/todos/:id                      TodoAppApiWeb.TodoController :update
+           PUT     /api/v1/todos/:id                      TodoAppApiWeb.TodoController :update
+todo_path  DELETE  /api/v1/todos/:id                      TodoAppApiWeb.TodoController :delete
+*/
+
+export const getApiV1Todos = () => "localhost:4000/api/v1/todos";
+export const postApiV1Todos = () => "localhost:4000/api/v1/todos";
+export const patchApiV1Todos = params =>
+  `localhost:4000/api/v1/todos/${params.id}`;
+export const deleteApiV1Todos = params =>
+  `localhost:4000/api/v1/todos/${params.id}`;
